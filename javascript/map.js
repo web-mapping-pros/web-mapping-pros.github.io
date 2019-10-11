c1d7ee6a127d67a92a7cda137fd8209a92c00079
DavidAMills <dam203@txstate.edu>
2019-10-11 11:16:28 -0500

    Reogranized CSS and Javascript.

---
+++ javascript/map.js
@@ -0,0 +1,9 @@
+var map = L.map('map').setView([29.8884, -97.9384], 14);
+
+mapLink =
+    '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+L.tileLayer(
+    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; ' + mapLink + ' Contributors',
+    maxZoom: 18,
+    }).addTo(map);
